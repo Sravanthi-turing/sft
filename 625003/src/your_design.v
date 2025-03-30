0f607d03146c43015111212143ac4be930fe7c4d
Sravanthi-turing <sravanthi.r@turing.com>
2025-03-30 18:31:39 +0530

    Update your_design.v

--- 625003/src/your_design.v
+++ 625003/src/your_design.v
@@ -17,24 +17,28 @@ module bus_arbiter #(
     reg [4:0] arb_counter;
     integer i;
 
-    always @(posedge clk or posedge reset) begin
-        if (reset) begin
-            num_masters <= NUM_MASTERS;
-            arb_counter <= 0;
-        end else if (config_wr) begin
-            case (config_addr)
-                2'b00: num_masters <= config_data[1:0];
-            endcase
-        end else begin
-            grant <= 0;
-            for (i = 0; i < num_masters; i = i + 1) begin
-                if (req[(arb_counter + i) % num_masters]) begin
-                    grant[(arb_counter + i) % num_masters] <= 1;
-                    arb_counter <= (arb_counter + i + 1) % num_masters;
-                    break;
-                end
+    
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        num_masters <= NUM_MASTERS;
+        arb_counter <= 0;
+        grant <= 0;
+    end else if (config_wr) begin
+        case (config_addr)
+            2'b00: num_masters <= config_data[1:0];
+        endcase
+    end else begin
+        reg [NUM_MASTERS-1:0] temp_grant;
+        temp_grant = 0;
+        for (i = 0; i < num_masters; i = i + 1) begin
+            if (req[(arb_counter + i) % num_masters]) begin
+                temp_grant[(arb_counter + i) % num_masters] = 1;
+                arb_counter <= (arb_counter + i + 1) % num_masters;
+                break;
             end
         end
+        grant <= temp_grant;
     end
+end
 
 endmodule
