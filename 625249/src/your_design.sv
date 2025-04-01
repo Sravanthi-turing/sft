3ca18594643ed54b36afdf033b5433fe5377b5f4
Sravanthi-turing <sravanthi.r@turing.com>
2025-04-01 23:23:56 +0530

    Update your_design.sv

--- 625249/src/your_design.sv
+++ 625249/src/your_design.sv
@@ -1,3 +1,4 @@
+
 module cache #(parameter ADDR_WIDTH = 32, DATA_WIDTH = 128, CACHE_SIZE = 1024) (
   input logic clk,
   input logic reset,
@@ -15,7 +16,7 @@ module cache #(parameter ADDR_WIDTH = 32, DATA_WIDTH = 128, CACHE_SIZE = 1024) (
     logic [DATA_WIDTH-1:0] data;
   } cache_line_t;
   
-  cache_line_t cache_mem [0:CACHE_SIZE-1];
+  cache_line_t cache_mem [CACHE_SIZE];
   
   always_ff @(posedge clk or posedge reset) begin
     if (reset) begin
@@ -23,23 +24,21 @@ module cache #(parameter ADDR_WIDTH = 32, DATA_WIDTH = 128, CACHE_SIZE = 1024) (
         cache_mem[i].valid <= 0;
       end
     end else if (write_en) begin
-      cache_mem[addr % CACHE_SIZE].valid <= 1;
-      cache_mem[addr % CACHE_SIZE].tag <= addr;
-      cache_mem[addr % CACHE_SIZE].data <= write_data;
+      cache_mem[$unsigned(addr) % CACHE_SIZE].valid <= 1;
+      cache_mem[$unsigned(addr) % CACHE_SIZE].tag <= addr;
+      cache_mem[$unsigned(addr) % CACHE_SIZE].data <= write_data;
     end
   end
   
   always_ff @(posedge clk) begin
+    hit <= 0;
     if (read_en) begin
-      if (cache_mem[addr % CACHE_SIZE].valid && cache_mem[addr % CACHE_SIZE].tag == addr) begin
+      if (cache_mem[$unsigned(addr) % CACHE_SIZE].valid && cache_mem[$unsigned(addr) % CACHE_SIZE].tag == addr) begin
         hit <= 1;
-        read_data <= cache_mem[addr % CACHE_SIZE].data;
+        read_data <= cache_mem[$unsigned(addr) % CACHE_SIZE].data;
       end else begin
-        hit <= 0;
         read_data <= '0;
       end
     end
   end
 endmodule
-
-
