189b631f54e0330e7fe151a14733f3b090994dd0
Sravanthi-turing <sravanthi.r@turing.com>
2025-04-02 11:10:01 +0530

    Update your_design.v

--- 625303/src/your_design.v
+++ 625303/src/your_design.v
@@ -1,5 +1,3 @@
-`timescale 1ns / 1ps
-
 module MemoryArray #( 
     parameter MEM_TYPE = "REG",  
     parameter DATA_WIDTH = 8,    
@@ -17,23 +15,41 @@ module MemoryArray #(
     generate
         if (MEM_TYPE == "REG") begin : REG_ARRAY
             reg [DATA_WIDTH-1:0] reg_mem [0:DEPTH-1];
+            integer i;
+            initial begin
+                for (i = 0; i < DEPTH; i = i + 1) 
+                    reg_mem[i] = 0;
+            end
             always @(posedge clk) begin
-                if (we) reg_mem[addr] <= din;
-                dout <= reg_mem[addr];
+                if (we && addr < DEPTH) reg_mem[addr] <= din;
+                if (addr < DEPTH) dout <= reg_mem[addr];
+                else dout <= {DATA_WIDTH{1'bx}};
             end
         end 
         else if (MEM_TYPE == "LUT") begin : LUT_ARRAY
             reg [DATA_WIDTH-1:0] lut_mem [0:DEPTH-1];
+            integer i;
+            initial begin
+                for (i = 0; i < DEPTH; i = i + 1) 
+                    lut_mem[i] = 0;
+            end
             always @(posedge clk) begin
-                if (we) lut_mem[addr] <= din;
-                dout <= lut_mem[addr];
+                if (we && addr < DEPTH) lut_mem[addr] <= din;
+                if (addr < DEPTH) dout <= lut_mem[addr];
+                else dout <= {DATA_WIDTH{1'bx}};
             end
         end 
         else if (MEM_TYPE == "BRAM") begin : BRAM_ARRAY
             reg [DATA_WIDTH-1:0] bram_mem [0:DEPTH-1];
+            integer i;
+            initial begin
+                for (i = 0; i < DEPTH; i = i + 1) 
+                    bram_mem[i] = 0;
+            end
             always @(posedge clk) begin
-                if (we) bram_mem[addr] <= din;
-                dout <= bram_mem[addr];
+                if (we && addr < DEPTH) bram_mem[addr] <= din;
+                if (addr < DEPTH) dout <= bram_mem[addr];
+                else dout <= {DATA_WIDTH{1'bx}};
             end
         end
         else begin
